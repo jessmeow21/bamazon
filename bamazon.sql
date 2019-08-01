27f76361692b0ee9ea31fdb2d17c9010768cded2
Jessica Sulaiman <jessicasulaiman@Jessicas-MacBook-Pro.local>
2019-08-01 09:26:28 -0400

    working table on mysql file

--- bamazon.sql
+++ bamazon.sql
@@ -5,12 +5,12 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-item_id INT NOT NULL AUTO_INCREMENT,
-product_name VARCHAR(30) NULL,
-department_name VARCHAR(30) NULL,
-price DECIMAL(10,2) NULL,
-stock_quantity INT NOT NULL,
-PRIMARY KEY (id)
+item_id INTEGER(10) AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(20) NOT NULL,
+department_name VARCHAR(20) NOT NULL,
+price DECIMAL(10,2) NOT NULL,
+stock_quantity INTEGER(20) NOT NULL,
+PRIMARY KEY (item_id)
 );
 
 
