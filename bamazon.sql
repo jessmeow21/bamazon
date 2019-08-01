551e9baa95f8d05ccdeb3345f2274cec1975ede8
Jessica Sulaiman <jessicasulaiman@Jessicas-MacBook-Pro.local>
2019-07-31 22:32:53 -0400

    updated inventory

--- bamazon.sql
+++ bamazon.sql
@@ -14,4 +14,32 @@ PRIMARY KEY (id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("desks", "Office", 25, 10);
+VALUES ("Desks", "Office", 25, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Chairs", "Office", 50, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Pencils", "Office", 100, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Protein Powder", "Health", 25, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Barbies", "Toys", 30, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Hot Wheels", "Toys", 25, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Toilet Paper", "Bath", 25, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Candles", "Home", 12, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Coffee", "Food", 10, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bedding", "Pets", 20, 50);
+
