9ebd4595af9996bc52cebf7727543fcf4d2bf417
Jessica Sulaiman <jessicasulaiman@Jessicas-MacBook-Pro.local>
2019-07-31 22:46:09 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # bamazon
+
+## About 
+This is a Amazon-like store front on the command line using Node and MySQL.
+
+This app will take in orders from customers and deplete stock 
--- bamazon.sql
+++ bamazon.sql
@@ -8,10 +8,11 @@ CREATE TABLE products (
 item_id INT NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(30) NULL,
 department_name VARCHAR(30) NULL,
-price INT NULL,
-stock_quantity INT NULL,
+price DECIMAL(10,2) NULL,
+stock_quantity INT NOT NULL,
 PRIMARY KEY (id)
 );
+
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Desks", "Office", 25, 10);
