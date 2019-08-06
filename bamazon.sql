8b8e2a37f942b518bebefc6186e7195cf83aa025
Jessica Sulaiman <jessica.sulaiman86@gmail.com>
2019-08-06 09:59:09 -0400

    cleaned up mysql workbench and screenshot product list from node

--- bamazon.sql
+++ bamazon.sql
@@ -15,32 +15,14 @@ PRIMARY KEY (item_id)
 
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Desks", "Office", 25, 10);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Chairs", "Office", 50, 20);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Pencils", "Office", 100, 10);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Protein Powder", "Health", 25, 50);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Barbies", "Toys", 30, 10);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Hot Wheels", "Toys", 25, 10);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Toilet Paper", "Bath", 25, 10);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Candles", "Home", 12, 15);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Coffee", "Food", 10, 100);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Bedding", "Pets", 20, 50);
+VALUES ("Desks", "Office", 25, 10),
+("Chairs", "Office", 50, 20),
+("Pencils", "Office", 100, 10),
+("Protein Powder", "Health", 25, 50),
+("Barbies", "Toys", 30, 10),
+("Hot Wheels", "Toys", 25, 10),
+("Toilet Paper", "Bath", 25, 10),
+("Candles", "Home", 12, 15),
+("Coffee", "Food", 10, 100),
+("Bedding", "Pets", 20, 50);
 
